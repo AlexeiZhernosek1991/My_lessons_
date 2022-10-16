61170bd2c360bc888878d04194d3f4d67044fcce
AlexeiZhernosek1991 <115401266+AlexeiZhernosek1991@users.noreply.github.com>
2022-10-16 21:58:02 +0300

    'lessons 13 №10'

--- practic/practic.py
+++ practic/practic.py
@@ -1,20 +1,47 @@
-# задача №5
-# def float_(a, b):
+# def tellefon_(text_: str):
+#     dict_alf = {
+#         ".": 1, ",": 11, "?": 111, "!": 1111, ":": 11111,
+#         "A": 2, "B": 22, "C": 22,
+#         "D": 3, "E": 33, "F": 33,
+#         "G": 4, "H": 44, "I": 444,
+#         "J": 5, "K": 55, "L": 555,
+#         "M": 6, "N": 66, "O": 666,
+#         "P": 7, "Q": 77, "R": 777, "S": 7777,
+#         "T": 8, "U": 88, "V": 888,
+#         "W": 9, "X": 99, "Y": 999, "Z": 9999
+#     }
+#     list_ = []
+#     for i in text_.upper():
+#         if i in dict_alf:
+#             list_.append(str(dict_alf[i]))
+#         elif i not in dict_alf:
+#             list_.append(i)
+#     print(list_)
 #
 #
-#
-# float_(12, 33)
-def div_(a,b):
-    division = a
-    s = 0
-    while division != 0:
-        if a % division == 0 and b % division == 0:
-            a = a/division
-            b = b/division
-            print(f'{a} /{b}')
-            division = 0
+# tellefon_('sdkm!fom,csn:o15817')
+
+
+
+def tellefon_(text_: str):
+    dict_alf = {
+        ".": 1, ",": 11, "?": 111, "!": 1111, ":": 11111,
+        "A": 2, "B": 22, "C": 22,
+        "D": 3, "E": 33, "F": 33,
+        "G": 4, "H": 44, "I": 444,
+        "J": 5, "K": 55, "L": 555,
+        "M": 6, "N": 66, "O": 666,
+        "P": 7, "Q": 77, "R": 777, "S": 7777,
+        "T": 8, "U": 88, "V": 888,
+        "W": 9, "X": 99, "Y": 999, "Z": 9999
+    }
+    for i in text_.upper():
+        if i in dict_alf:
+            print(str(dict_alf[i]), end=" ")
+        elif i.isdigit():
+            print(i, end=" ")
         else:
-            division -= 1
+            print(end='')
 
-div_(12, 33)
 
+tellefon_('sdkm!fom,csn:(o1581)7')
--- practic/practic_lessons_13.py
+++ practic/practic_lessons_13.py
@@ -136,3 +136,27 @@ def data(day, month, age):
         print(f'{1}, {month + 1}', {age})
     elif 0 < day < 32:
         print(f'{day + 1}, {month}, {age}')
+
+# №10
+def tellefon_(text_: str):
+    dict_alf = {
+        ".": 1, ",": 11, "?": 111, "!": 1111, ":": 11111,
+        "A": 2, "B": 22, "C": 22,
+        "D": 3, "E": 33, "F": 33,
+        "G": 4, "H": 44, "I": 444,
+        "J": 5, "K": 55, "L": 555,
+        "M": 6, "N": 66, "O": 666,
+        "P": 7, "Q": 77, "R": 777, "S": 7777,
+        "T": 8, "U": 88, "V": 888,
+        "W": 9, "X": 99, "Y": 999, "Z": 9999
+    }
+    for i in text_.upper():
+        if i in dict_alf:
+            print(str(dict_alf[i]), end=" ")
+        elif i.isdigit():
+            print(i, end=" ")
+        else:
+            print(end='')
+
+
+tellefon_('sdkm!fom,csn:(o1581)7')
