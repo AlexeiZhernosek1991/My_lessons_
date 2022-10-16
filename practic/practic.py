fa7e7c21091333bda09abdddb308f34b2a219e4a
AlexeiZhernosek1991 <115401266+AlexeiZhernosek1991@users.noreply.github.com>
2022-10-16 20:58:47 +0300

    'lessons_13'

--- practic/practic.py
+++ practic/practic.py
@@ -1,61 +1,20 @@
-# def letter_(string_: str):
-#     letter_a = 0
-#     letter_b = 0
-#     list_1 = ['а', 'о', 'у', 'э', 'ы', 'я', 'е', 'ё', 'ю', 'и']
-#     for letter in string_.lower():
-#         if letter in list_1:
-#             letter_a +=1
-#         else:
-#             letter_b +=1
-#     print(f'гласных букв {letter_a}, согласных букв {letter_b}')
-#
-# letter_(input('введите текст >>>'))
-# def sum_num_(num_):   # объявленеие функции  и парамметров
-#     sum_num = num_ + num_**2 + num_**3   # слажение введенного числа с возведением его в степени
-#     print(sum_num)  # ввывод значения
+# задача №5
+# def float_(a, b):
 #
 #
-# sum_num_(int(input('введите число >>>')))  # вызов функции
-
-# def func_y(x):
-#     y = 0
-#     if -5 <= x <= 5:
-#         y = x ** 2
-#     elif x < -5:
-#         y = 2 * abs(x) - 1
-#     elif x > 5:
-#         y = 2 * x
-#     return y
 #
-#
-# for x in range(-10, 11):
-#     print(func_y(x))
+# float_(12, 33)
+def div_(a,b):
+    division = a
+    s = 0
+    while division != 0:
+        if a % division == 0 and b % division == 0:
+            a = a/division
+            b = b/division
+            print(f'{a} /{b}')
+            division = 0
+        else:
+            division -= 1
 
-def list_tuple(x):
-    sum_word = 0  # щетчик для слов
-    sum_letter = 0  # щетчик для букв
-    sum_num = 0  # щетчик для нечетных цифр
-    if type(x) is tuple:  # проверка на тип кортеж
-        for word in x:  # цикл для извлечения объектов кортежа
-            if type(word) is str and word.isalpha():  # проверка на тип строка которая ссостоит из букв
-                sum_word += 1  # щетчик объектов слова
-        return sum_word  # возврат количества слов в кортеже
-    elif type(x) is list:  # проверка на тип список
-        for string_ in x:  # цик для извлечения объектов списка
-            if type(string_) is str and string_.isalpha():  # проверка на тип строка которая состоит из букв
-                sum_letter += len(string_)  # щетчик для всех букв
-            elif type(string_) is int or \
-                    type(string_) is str and string_.isdigit():  # проверка на тип число или строка состоящая из чисел
-                for num_ in str(string_):  # цикл для переборки строки с цифрами
-                    if int(num_) % 2 != 0:  # выбор нечетных цифр
-                        sum_num += 1  # подсчет нечетных цифр
-        return sum_letter, sum_num  # возврат количества букв, количество нечетных цифр
-
-
-tuple_1 = ('age', 1991, 'month', 5, 'day', '30')  # заданный кортеж
-list_1 = ['age', 1991, 'month', 5, 'day', '30']  # заданный список
-
-print(type(tuple_1), list_tuple(tuple_1))  # проверка с кортежем
-print(type(list_1), list_tuple(list_1))  # проверка с списком
-
+div_(12, 33)
 
---
+++ practic/practic_lessons_13.py
@@ -0,0 +1,138 @@
+# № 7
+def countrange(list_, a: float, b: float) -> int:
+    sum_i = 0
+    for i in list_:
+        if a < float(i) < b:
+            sum_i += 1
+    return sum_i
+
+
+list_7 = [0, 5, 9, -1, -8, 5, 5, -9, 1]
+
+print(countrange(list_7, 0, 5))
+print(countrange(list_7, -1.5, 10))
+print(countrange(list_7, -10, 10))
+
+
+# № 8
+def list_in_list(list_: list):
+    list_sum = 0
+    for list_in in list_:
+        if type(list_in) == list:
+            list_sum += 1
+    print(list_sum)
+
+
+list_8 = [{0, 5, 9}, [1, -8], [2, 3, 4, 5], (5, 5, -9, 1)]
+
+list_in_list(list_8)
+
+
+# № 9
+
+def word_anagram(word1: str, word2: str):
+    print(word1[::-1] == word2)
+
+
+word_anagram('life', 'efil')
+word_anagram('life', 'ufo')
+
+
+# № 6
+def list_6_(list_):
+    print(list_[::-1])
+    # print(list_.sort)
+    # print(list_.sort(reverse=True))
+    print(list_[1:7])
+    list_.pop(4)
+    print(list_)
+    print(set(list_))
+    list_new = []
+    for i in list_:
+        if type(i) == str:
+            list_new.append(i)
+    print(list_new)
+
+
+list_6 = [1, 6, 5, 3, 4, 'as', 'ef', 'w', 'a', 'w']
+
+list_6_(list_6)
+
+
+# № 1
+def bmi_(w, h):
+    my_bmi = w / h ** 2
+    if my_bmi < 18.5:
+        print('Дефицит массы тела')
+    elif 18.5 < my_bmi < 24.9:
+        print('Нормальная масса тела')
+    elif 25 < my_bmi < 29.9:
+        print('Увеличение массы тела')
+    elif 30 < my_bmi < 34.9:
+        print('Ожирение 1 степени')
+    elif 35 < my_bmi < 39.9:
+        print('Ожирение 2 степени')
+    elif my_bmi >= 40:
+        print('Ожирение 3 степени')
+
+
+bmi_(105, 1.76)
+
+
+# № 2
+def figure_(side):
+    if side == 3:
+        print('треугольник')
+    elif side == 4:
+        print('четырехугольник')
+    elif side == 5:
+        print('пятиугольник')
+    elif side == 6:
+        print('шестиугольник')
+    elif side == 7:
+        print('септагон')
+    elif side == 8:
+        print('октогон')
+    elif side == 9:
+        print('ноногон')
+    elif side == 10:
+        print('десятиугольник')
+    else:
+        print('вы вышли за допустимый диапазон от 3 до 10 сторон')
+
+
+figure_(5)
+
+
+# № 5
+
+def div_(a, b):
+    division = a
+    s = 0
+    while division != 0:
+        if a % division == 0 and b % division == 0:
+            a = a / division
+            b = b / division
+            print(f'{a} /{b}')
+            division = 0
+        else:
+            division -= 1
+
+
+div_(12, 33)
+
+
+# № 4
+def data(day, month, age):
+    if day == 31 and month == 12:
+        print(f'{1},{1},{age + 1}')
+    elif month == 2 and day == 28:
+        print(f'{1},{3},{age}')
+    elif month == 2 and day == 29 and age % 4 == 0:
+        print(f'{1},{3},{age}')
+    elif month == 4 or 9 or 6 or 11 and day == 30:
+        print(f'{1}, {month}, {age}')
+    elif day == 31:
+        print(f'{1}, {month+1}', {age})
+    else:
+        print(f'{day+1}, {month}, {age}')
