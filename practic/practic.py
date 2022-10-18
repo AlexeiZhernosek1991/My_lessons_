f16877c42bb83e42aa4dc076070d19acd0844491
AlexeiZhernosek1991 <115401266+AlexeiZhernosek1991@users.noreply.github.com>
2022-10-18 22:39:58 +0300

    'lessons 13 №11'

--- practic/practic.py
+++ practic/practic.py
@@ -1,18 +1,30 @@
-def data(day, month, age):
-    if day == 31 and month == 12:
-        print(f'{1},{1},{age + 1}')
-    elif month == 2 and day == 28 and age % 4 == 0:
-        print(f'{29},{2},{age}')
-    elif month == 2 and day == 28 and age % 100 == 0 and age % 400:
-        print(f'{29},{2},{age}')
-    elif month == 2 and day == 28:
-        print(f'{1},{3},{age}')
-    elif month in [4, 9, 11, 6] and day == 30:
-        print(f'{1}, {month + 1}, {age}')
-    elif day == 31:
-        print(f'{1}, {month + 1}, {age}')
-    elif 0 < day < 31:
-        print(f'{day + 1}, {month}, {age}')
+def alingment(list_):
+    l_1 = []
+    while len(list_) < 9:
+        for i in list_:
+            if type(i) is not list:
+                l_1.append(i)
+            elif type(i) is list:
+                l_1.extend(i)
+        list_ = l_1
+        l_1 = []
+    for i in list_:
+        if type(i) is not list:
+            l_1.append(i)
+        elif type(i) is list:
+            l_1.extend(i)
+    list_ = l_1
+    print(list_)
 
 
-data(28, 2, 1993)
+def alingment(s):
+    if s == []:
+        return s
+    elif type(s[0]) == list:
+        return (alingment(s[0]) + alingment(s[1:]))
+    return (s[:1] + alingment(s[1:]))
+
+
+list_1 = [[1], [2, 3], [[4, 5], 6], [7, [8, [9]]]]
+
+print(fff(list_1))
--- practic/practic_lessons_13.py
+++ practic/practic_lessons_13.py
@@ -178,3 +178,45 @@ def delivery_(a):
 
 
 print(delivery_(int(input('введите количества товара >>>'))))
+
+
+# №11
+def alingment(s):
+    if s == []:
+        return s
+    elif type(s[0]) == list:
+        return (alingment(s[0]) + alingment(s[1:]))
+    return (s[:1] + alingment(s[1:]))
+
+
+list_1 = [[1], [2, 3], [[4, 5], 6], [7, [8, [9]]]]
+
+print(alingment(list_1))
+
+
+# №11  вариант 2
+
+def xxx(list_):
+    l_1 = []
+    while len(list_) < 9:
+        for i in list_:
+            if type(i) is not list:
+                l_1.append(i)
+            elif type(i) is list:
+                l_1.extend(i)
+        list_ = l_1
+        l_1 = []
+    for i in list_:
+        if type(i) is not list:
+            l_1.append(i)
+        elif type(i) is list:
+            l_1.extend(i)
+    list_ = l_1
+    print(list_)
+
+
+list_1 = [[1], [2, 3], [[4, 5], 6], [7, [8, [9]]]]
+
+xxx(list_1)
+
+
