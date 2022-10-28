d760aaecab880b5fdf4311551dc8bbcb4a03f9d9
AlexeiZhernosek1991 <115401266+AlexeiZhernosek1991@users.noreply.github.com>
2022-10-28 19:57:41 +0300

    'lessons 16'

--- NEW/practic_104_lessons_15.py
+++ NEW/practic_104_lessons_15.py
@@ -26,12 +26,12 @@ import os
 #         sum_i += int(i)
 # print(sum_i)
 
-# д.з.
-"""Исходный список"""
-list_ = [1, 'my', 'school', 5, 3]
-"""Создаем строку с отсортированными цифрами по возростанию"""
-string_1 = ' '.join(sorted([str(i) for i in list_ if isinstance(i, int)]))
-"""Создаем строку с отсортированными строками по длинне строки"""
-string_a = ' '.join(sorted([i for i in list_ if isinstance(i, str)]))
-with open('test.txt', 'w') as file_test:  # открыть указаный файл для редоктирования
-    file_test.write(string_a + ' ' + string_1)  # вписываем строку в файл
+# # д.з.
+# """Исходный список"""
+# list_ = [1, 'my', 'school', 5, 3]
+# """Создаем строку с отсортированными цифрами по возростанию"""
+# string_1 = ' '.join(sorted([str(i) for i in list_ if isinstance(i, int)]))
+# """Создаем строку с отсортированными строками по длинне строки"""
+# string_a = ' '.join(sorted([i for i in list_ if isinstance(i, str)]))
+# with open('test.txt', 'w') as file_test:  # открыть указаный файл для редоктирования
+#     file_test.write(string_a + ' ' + string_1)  # вписываем строку в файл
