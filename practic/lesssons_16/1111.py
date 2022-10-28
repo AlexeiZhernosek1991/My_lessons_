41a8be6d28b8c20fa3b79bcbc834a71a18f5b9c6
AlexeiZhernosek1991 <115401266+AlexeiZhernosek1991@users.noreply.github.com>
2022-10-28 20:00:42 +0300

    'change dir'

--- practic/lesssons_16/1111.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('example.csv', encoding='UTF-8') as csv_file:
-    exampleReader = csv.reader(csv_file)
-    for row in exampleReader:
-        string = 'Строка #' + str(exampleReader.line_num) + ' '
-        for value in row:
-            string += value + ' '
-        print(string)
