b2748b8a0eb23c2f0771369fb8c85448c08dbfb7
AlexeiZhernosek1991 <115401266+AlexeiZhernosek1991@users.noreply.github.com>
2022-11-20 00:37:39 +0300

    'parser 508 aisico'

--- practic/practic.py
+++ practic/practic.py
@@ -1,67 +1,149 @@
-import requests
+import json
+import time
 from bs4 import BeautifulSoup
+import datetime
 import csv
-import string
+import asyncio
+import aiohttp
 
 
-def wriner_(list_):
-    with open('pars.csv', 'w', encoding='cp1251', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow(list_)
+books_data = []
+start_time = time.time()
 
 
-def main(url, head):
-    with open('pars.csv', 'w', encoding='utf8', newline='') as file:
-        writer_ = csv.writer(file, delimiter=';')
-        writer_.writerow(['transmission', 'engine volume', 'fuel', 'price', 'price_usd', 'type', 'location', 'http'])
-    pages = int(input('введите количество страниц >>>'))
-    for page in range(1, pages + 1):
-        response = get_html(url + page)
-        pars(response, head)
+async def get_page_data(session, page):
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"
+    }
+
+    url = f"https://www.labirint.ru/genres/2308/?available=1&paperbooks=1&display=table&page={page}"
+
+    async with session.get(url=url, headers=headers) as response:
+        response_text = await response.text()
+
+        soup = BeautifulSoup(response_text, "lxml")
+
+        books_items = soup.find("tbody", class_="products-table__body").find_all("tr")
+
+        for bi in books_items:
+            book_data = bi.find_all("td")
+
+            try:
+                book_title = book_data[0].find("a").text.strip()
+            except:
+                book_title = "Нет названия книги"
+
+            try:
+                book_author = book_data[1].text.strip()
+            except:
+                book_author = "Нет автора"
+
+            try:
+                book_publishing = book_data[2].find_all("a")
+                book_publishing = ":".join([bp.text for bp in book_publishing])
+            except:
+                book_publishing = "Нет издательства"
+
+            try:
+                book_new_price = int(book_data[3].find("div", class_="price").find("span").find("span").text.strip().replace(" ", ""))
+            except:
+                book_new_price = "Нет нового прайса"
+
+            try:
+                book_old_price = int(book_data[3].find("span", class_="price-gray").text.strip().replace(" ", ""))
+            except:
+                book_old_price = "Нет старого прайса"
+
+            try:
+                book_sale = round(((book_old_price - book_new_price) / book_old_price) * 100)
+            except:
+                book_sale = "Нет скидки"
+
+            try:
+                book_status = book_data[-1].text.strip()
+            except:
+                book_status = "Нет статуса"
+
+            books_data.append(
+                {
+                    "book_title": book_title,
+                    "book_author": book_author,
+                    "book_publishing": book_publishing,
+                    "book_new_price": book_new_price,
+                    "book_old_price": book_old_price,
+                    "book_sale": book_sale,
+                    "book_status": book_status
+                }
+            )
+
+        print(f"[INFO] Обработал страницу {page}")
 
 
-with open('pars.csv', 'w', encoding='utf8', newline='') as file:
-    writer_ = csv.writer(file, delimiter=';')
-    writer_.writerow(['num', 'transmission', 'engine volume', 'fuel', 'price', 'price_usd', 'type', 'location', 'http'])
+async def gather_data():
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"
+    }
 
-url_ = 'https://cars.av.by/filter?brands[0][brand]=989&brands[0][model]=2262&brands[0][generation]=2033&page='
-head_ = 'https://cars.av.by'
+    url = "https://www.labirint.ru/genres/2308/?available=1&paperbooks=1&display=table"
 
-response = requests.get(url_)
-soup = BeautifulSoup(response.text, 'lxml')
-cars_ = soup.find_all('div', class_='listing-item')
-list_car = []
-for car in cars_:
-    list_car.append(
-        {'transmission': car.find('div', class_='listing-item__params').find_all('div')[1].text.split(',')[0],
-         'engine volume': car.find('div', class_='listing-item__params') .find_all('div')[1].text.split(',')[1],
-         'fuel': car.find('div', class_='listing-item__params').find_all('div')[1].text.split(',')[2],
-         'type': car.find('div', class_='listing-item__params').find_all('div')[1].text.split(',')[3],
-         'price': car.find('div', class_='listing-item__price').text,
-         'price_usd': car.find('div', class_='listing-item__priceusd').text,
-         'location': car.find('div', class_='listing-item__location').text,
-         'http': head_ + car.find('a', class_='listing-item__link').get('href')
-         }
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url=url, headers=headers)
+        soup = BeautifulSoup(await response.text(), "lxml")
+        pages_count = int(soup.find("div", class_="pagination-numbers").find_all("a")[-1].text)
 
-    )
-print(list_car)
-list_new = []
-for car in list_car:
-    list_new.append([car['transmission'], car['engine volume'], car['fuel'], car['type'],
-                     car['price'], car['price_usd'], car['location'], car['http']])
-print(list_new)
-print(list_new[2][1])
-#
-# with open('pars.csv', 'w', encoding='utf8', newline='') as file:
-#     writer_ = csv.writer(file, delimiter=';')
-#     for car in list_car:
-#         writer_.writerow([car['transmission'], car['engine volume'], car['fuel'], car['type'],
-#                           car['price'], car['price_usd'], car['location'], car['http']])
+        tasks = []
+
+        for page in range(1, pages_count + 1):
+            task = asyncio.create_task(get_page_data(session, page))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
 
 
-# with open('pars.csv', 'r', encoding='utf8', newline='') as file:
-#     reader_ = csv.reader(file, delimiter=';')
-#     lit_11 = list(reader_)
-#     a = lit_11[0][4]
-#     print(lit_11)
-#
+def main():
+    asyncio.run(gather_data())
+    cur_time = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
+
+    with open(f"labirint_{cur_time}_async.json", "w") as file:
+        json.dump(books_data, file, indent=4, ensure_ascii=False)
+
+    with open(f"labirint_{cur_time}_async.csv", "w") as file:
+        writer = csv.writer(file)
+
+        writer.writerow(
+            (
+                "Название книги",
+                "Автор",
+                "Издательство",
+                "Цена со скидкой",
+                "Цена без скидки",
+                "Процент скидки",
+                "Наличие на складе"
+            )
+        )
+
+    for book in books_data:
+
+        with open(f"labirint_{cur_time}_async.csv", "a") as file:
+            writer = csv.writer(file)
+
+            writer.writerow(
+                (
+                    book["book_title"],
+                    book["book_author"],
+                    book["book_publishing"],
+                    book["book_new_price"],
+                    book["book_old_price"],
+                    book["book_sale"],
+                    book["book_status"]
+                )
+            )
+
+    finish_time = time.time() - start_time
+    print(f"Затраченное на работу скрипта время: {finish_time}")
+
+
+if __name__ == "__main__":
+    main()
